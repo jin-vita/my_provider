dabac3efcedf3939e43a1d9675363d1120f89918
jin-vita <vita.unsnetworks@gmail.com>
2023-02-04 14:14:41 +0900

    provider_test

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'provider/user.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => User(),
+        )
+      ],
+      child: const MaterialApp(
+        home: MyHomePage(title: '내가 만든 앱'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  void changeName(String name) {
+    User user = Provider.of(context, listen: false);
+    user.count ??= 0;
+    var count = user.count! + 1;
+    user.applyData(name: name, count: count);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        color: Colors.yellow,
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Consumer<User>(
+                builder: (context, data, child) {
+                  return Text('이름 : ${data.name}, 클릭 수 : ${data.count}');
+                },
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: ElevatedButton.icon(
+                icon: const Icon(
+                  Icons.add,
+                  size: 18,
+                ),
+                label: const Text('확인'),
+                onPressed: () {
+                  changeName('홍길동');
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/provider/user.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class User extends ChangeNotifier {
+  String? name;
+  int? count;
+
+  User({this.name, this.count});
+
+  void setData({name, count}) {
+    this.name = name;
+    this.count = count;
+  }
+
+  void applyData({name, count}) {
+    setData(name: name, count: count);
+    notifyListeners();
+  }
+}
